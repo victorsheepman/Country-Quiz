bd0fcbe4b04886fcf6e735866d772e905908c9f7
victor_marquez <victorsheepman@gmail.com>
2023-12-07 17:58:32 -0400

    add more questions

--- src/mock/questionList.ts
+++ src/mock/questionList.ts
@@ -44,5 +44,45 @@ export const questionsList:Array<Question> = [
                 title:'Austria'
             }
         ]
-    }
+    },
+    {
+        question: 'What is the currency of Japan?',
+        answer: 'C',
+        options: [
+          { letter: 'A', title: 'Yuan' },
+          { letter: 'B', title: 'Won' },
+          { letter: 'C', title: 'Yen' },
+          { letter: 'D', title: 'Ringgit' },
+        ],
+      },
+      {
+        question: 'In which continent is Egypt located?',
+        answer: 'A',
+        options: [
+          { letter: 'A', title: 'Africa' },
+          { letter: 'B', title: 'Asia' },
+          { letter: 'C', title: 'Europe' },
+          { letter: 'D', title: 'South America' },
+        ],
+      },
+      {
+        question: 'What is the capital city of Australia?',
+        answer: 'D',
+        options: [
+          { letter: 'A', title: 'Sydney' },
+          { letter: 'B', title: 'Melbourne' },
+          { letter: 'C', title: 'Canberra' },
+          { letter: 'D', title: 'Canberra' },
+        ],
+      },
+      {
+        question: 'Which planet is known as the Red Planet?',
+        answer: 'B',
+        options: [
+          { letter: 'A', title: 'Jupiter' },
+          { letter: 'B', title: 'Mars' },
+          { letter: 'C', title: 'Venus' },
+          { letter: 'D', title: 'Saturn' },
+        ],
+      },
 ]
